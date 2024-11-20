f0058577d2eb36958775636d31cbe2f312cea72e
Schuh <500190471@fvtc.edu>
2024-11-20 11:58:38 -0600

    updated

--- site.js
+++ site.js
@@ -5,16 +5,21 @@ const button = document.querySelector('button')
 
 const todos = JSON.parse(localStorage.getItem('todo-list')) || []
 
-const renderTodos = 
+const renderTodos = todos.map(( input ) => {
+
+    const li = document.createElement('li')
+    li.innerHTML = ''
+    li.textContent = todos.text
+    input.append(li)
+})
 
 button.addEventListener('click', () => {
     todos.push({ text: input.value, completed: false })
-    p.textContent = input.value
+    localStorage.setItem('todo-list', JSON.stringify(todos))
+
 })
 
-[
-    { "text": "Buy milk", "completed": false },
-    { "text": "Walk the dog", "completed": false },
-    { "text": "Do homework", "completed": false }
-]
+    
+    
 
+    

69a8d201c458137e1ca89a5978002164c4cdacb6
Schuh <500190471@fvtc.edu>
2024-11-21 15:41:19 -0600

    Most part done

--- site.js
+++ site.js
@@ -2,24 +2,21 @@
 const item = 'To-Do List Item'
 const input = document.querySelector('input')
 const button = document.querySelector('button')
+const todolist = document.querySelector('ul')
 
 const todos = JSON.parse(localStorage.getItem('todo-list')) || []
 
-const renderTodos = todos.map(( input ) => {
+todolist.innerHTML = ''
 
+const renderTodos = todos.map(( todos ) => {
     const li = document.createElement('li')
-    li.innerHTML = ''
     li.textContent = todos.text
-    input.append(li)
+    todolist.append(li)
+
 })
 
 button.addEventListener('click', () => {
     todos.push({ text: input.value, completed: false })
     localStorage.setItem('todo-list', JSON.stringify(todos))
-
+    
 })
-
-    
-    
-
-    
